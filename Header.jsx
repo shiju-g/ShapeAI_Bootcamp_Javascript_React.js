29b3269332b268e75a2084026c927c0f9bc1e6ac
shiju-g <85386417+shiju-g@users.noreply.github.com>
2021-06-05 12:42:48 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="head">
+      <h1>Shape AI</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h3> Javascript & React.js </h3>
+      <p>
+        This was an amazing bootcamp taken by shaurya sinha and we covered
+        everything from scratch including javascript,react.js,html
+      </p>
+    </div>
+  );
+}
+
+export default Note;
